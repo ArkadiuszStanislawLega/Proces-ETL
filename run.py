c534ec4dfaa223518a21cb65995fec63eaf73321
Arkadiusz Stanisław Łęga <arkadiusz.stanislaw.lega@gmail.com>
2020-03-26 18:30:57 +0100

    Starting test

--- run.py
+++ run.py
@@ -1,5 +1,38 @@
+from argparse import ArgumentParser
+from sqlite3 import connect
+
+worker_table_stmt = """
+    CREATE TABLE IF NOT EXIST worker(
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name VARCHAR(20),
+        salary INTEGER
+    )"""
+
+insert_stmt = 'INSERT INTO worker(name, salary) VALUES(?,?)'
+data_to_isert = [
+    ('name1', 100),
+    ('name2', 200),
+    ('name3', 300),
+    ('name4', 400),
+    ('name5', 500),
+    ('name6', 600)
+]
+
+
 def main():
-    pass
+    parser = ArgumentParser(description='This is an example of sql API')
+    parser.add_argument('--path', dest='path', type=str, required=True)
+
+    args = parser.parse_args()
+
+    with connect(args.path) as db_connctor:
+        db_connctor.execute(worker_table_stmt)
+
+        db_cursor = db_connctor.cursor()
+        db_cursor.executemany(insert_stmt, data_to_isert)
+
+        for entry in db_cursor.execute('SELECT * FROM worker'):
+            print(entry)
 
 
 if __name__ == "__main__":
